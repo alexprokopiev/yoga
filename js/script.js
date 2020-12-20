4e70591a4ef4f392cfebdb0d9cd7aca076145983
Alex Prokopiev <alexprokopiev86@gmail.com>
2020-12-20 19:43:21 +0500

    add modal

--- js/script.js
+++ js/script.js
@@ -83,4 +83,31 @@ window.addEventListener("DOMContentLoaded", function () {
   }
 
   setClock("timer", deadline);
+
+  //Modal
+
+  let more = document.querySelector(".more"),
+    descrBtn = document.querySelectorAll(".description-btn"),
+    overlay = document.querySelector(".overlay"),
+    close = document.querySelector(".popup-close");
+
+  more.addEventListener("click", function () {
+    overlay.style.display = "block";
+    this.classList.add("more-splash");
+    document.body.style.overflow = "hidden";
+  });
+
+  descrBtn.forEach(function (item) {
+    item.addEventListener("click", function () {
+      overlay.style.display = "block";
+      this.classList.add("more-splash");
+      document.body.style.overflow = "hidden";
+    });
+  });
+
+  close.addEventListener("click", function () {
+    overlay.style.display = "none";
+    more.classList.remove("more-splash");
+    document.body.style.overflow = "";
+  });
 });
